159ebeb9fb7d03b04a9062208375d203637be858
Ihor <ihor.zhelezniak@mths.ca>
2022-12-21 01:21:29 +0000

    -

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+/**
+ * This is programm
+ * By:      Igor
+ * Version: 1.0
+ * Since:   2022-10-13
+ */
+
+import Airplane from './Airplane'
+import Jet from './Jet'
+
+const one = 212
+const two = 422
+const three = 4
+const four = 5000
+
+const biplane = new Airplane()
+biplane.setSpeed(one)
+console.log(biplane.getSpeed())
+
+const boeing = new Jet()
+boeing.setSpeed(two)
+console.log(boeing.getSpeed())
+let counter = 0
+
+while (counter < three) {
+  boeing.accelerate()
+  console.log(boeing.getSpeed())
+
+  if (boeing.getSpeed() > four) {
+    biplane.setSpeed(biplane.getSpeed() * 2)
+  } else {
+    boeing.accelerate()
+  }
+  counter++
+}
+
+console.log(biplane.getSpeed())
+
